3e33e997e60740036fd98ab11e903a56444e5303
Babiac Marcel <SergiuRomaniuc>
2025-02-19 16:49:04 +0200

    Second Commit. work on App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,25 +3,38 @@ import './App.css';
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
-
-function App() {
-  return (
-  <div>
-    <h1>Ja<span className="highlight">mmm</span>ing</h1>
-    <div className="App">
-      {/*<!-- Add a SearchBar component -->*/}
-      <SearchBar />
-      <div className="App-playlist">
-        {/*<!-- Add a SearchResults component -->*/}
-        <SearchResults />
-        {/*<!-- Add a Playlist component -->*/}
-        <Playlist />
-      </div>
-    </div>
-  </div>
-  );
+import React from 'react';
 
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state.searchReults = [
+      {name: "Tiny Dancer", artist: "Elton John", album: "Madman Across The Water"},
+      {name: "Little Dancer", artist: " Tim McGraw", album: "Love Story"},
+      {name: "Big Dancer", artist: "Rockabye", album: "Lullaby Rendictions of Elotn John"},
+      {name: "Small Dancer", artist: "The White Raven", album: "Small but Big"},
+      {name: "", artist: "", album: ""}
+    ];
+  }
+
+  render() {
+    return (
+        <div>
+          <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div className="App">
+            {/*<!-- Add a SearchBar component -->*/}
+            <SearchBar />
+            <div className="App-playlist">
+              {/*<!-- Add a SearchResults component -->*/}
+              <SearchResults />
+              {/*<!-- Add a Playlist component -->*/}
+              <Playlist />
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
