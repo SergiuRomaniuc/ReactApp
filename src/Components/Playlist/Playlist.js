d7301d5c65fd25cd855d94cbdd663b7bef00404c
Babiac Marcel <SergiuRomaniuc>
2025-02-19 15:45:37 +0200

    first commit

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,14 @@
+import './Playlist.css'
+import TrackList from '../TrackList/TrackList';
+function Playlist() {
+    return (
+        <div className="Playlist">
+            <input defaultValue={'New Playlist'}/>
+            {/*<!-- Add a TrackList component -->*/}
+            <TrackList />
+            <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        </div>
+    );
+}
+
+export default Playlist;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import './TrackList.css'
+
+function TrackList() {
+    return (
+        <div className="TrackList">
+            {/*// <!-- You will add a map method that renders a set of Track components  -->*/}
+            Song 1
+            Song 2
+            Song 3
+        </div>
+    );
+}
+
+export default TrackList;
